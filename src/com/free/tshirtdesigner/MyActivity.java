941bd0c82dee62f7dfd37fba92793b4a9e2adff3
Nguyen Tuan Anh <anhnt@qsoft.com.vn>
2014-02-22 17:34:37 +0700

    main layout design

--- src/com/free/tshirtdesigner/MyActivity.java
+++ src/com/free/tshirtdesigner/MyActivity.java
@@ -2,16 +2,37 @@ package com.free.tshirtdesigner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyActivity extends Activity
 {
-    /**
-     * Called when the activity is first created.
-     */
+    ImageView ivShirt;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ivShirt = (ImageView) findViewById(R.id.ivShirt);
+        ivShirt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                switch (motionEvent.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return false;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
     }
 }
