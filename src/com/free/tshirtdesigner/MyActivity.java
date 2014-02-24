799fb05dbe0063c4a258f551fe3a74e19910eaea
thinhdd <mr.thinhdaoduc@gmail.com>
2014-02-24 22:55:41 +0700

    commit move image

--- src/com/free/tshirtdesigner/MyActivity.java
+++ src/com/free/tshirtdesigner/MyActivity.java
@@ -1,70 +1,179 @@
 package com.free.tshirtdesigner;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.view.ViewGroup;
+import android.widget.*;
+import com.free.tshirtdesigner.util.UtilImage;
 
 import static android.widget.RelativeLayout.LayoutParams;
 
-public class MyActivity extends Activity
-{
+public class MyActivity extends Activity {
     private ImageView ivShirt;
     private int home_x, home_y;
     private LayoutParams layoutParams;
+    private Button btSetting;
+    private UtilImage utilImage;
+    private Button btGetImageGallery;
+    private RelativeLayout shapeLayout;
+    private ImageView ivImageShow;
+    private ImageView ivResizeBottom;
+    private ImageView ivResizeTop;
+    RelativeLayout rlRootLayout;
+
+    private String colors="white";
+    private int tShirtDirection;
+    private int _yDelta;
+    private int _xDelta;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        ivShirt = (ImageView) findViewById(R.id.ivShirt);
+        setUpViewById();
+        utilImage = new UtilImage();
+        btGetImageGallery.setOnClickListener(onClickListener);
         ivShirt.setImageResource(R.drawable.tshirt_front_500);
-        ivShirt.setOnTouchListener(new View.OnTouchListener()
-        {
+        ivShirt.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent)
-            {
+            public boolean onTouch(View view, MotionEvent motionEvent) {
                 return onTouchShirt(view, motionEvent);
             }
         });
 
         RadioGroup rgShirtViewType = (RadioGroup) findViewById(R.id.rgShirtViewType);
-        rgShirtViewType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(RadioGroup group, int checkedId)
-            {
-                switch (group.getCheckedRadioButtonId())
-                {
+        rgShirtViewType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
                     case R.id.rbLeftSide:
-                        ivShirt.setImageResource(R.drawable.tshirt_left_500);
+                        showDirectionTShirt(R.drawable.tshirt_left_500);
                         break;
                     case R.id.rbBackSide:
-                        ivShirt.setImageResource(R.drawable.tshirt_back_500);
+                        showDirectionTShirt(R.drawable.tshirt_back_500);
                         break;
                     case R.id.rbFrontSide:
-                        ivShirt.setImageResource(R.drawable.tshirt_front_500);
+                        showDirectionTShirt(R.drawable.tshirt_front_500);
                         break;
                     case R.id.rbRightSide:
-                        ivShirt.setImageResource(R.drawable.tshirt_right_500);
+                        showDirectionTShirt(R.drawable.tshirt_right_500);
                         break;
                 }
             }
         });
 
+        btSetting.setOnClickListener(onClickListener);
+
         RadioButton rbFront = (RadioButton) findViewById(R.id.rbFrontSide);
         rbFront.setChecked(true);
     }
 
-    private boolean onTouchShirt(View view, MotionEvent motionEvent)
+    private void setUpViewById() {
+        shapeLayout = (RelativeLayout) getLayoutInflater().inflate(R.layout.shape_layout, null);
+        rlRootLayout = (RelativeLayout) findViewById(R.id.main_activity_rlShowTShirt);
+        ivShirt = (ImageView) findViewById(R.id.ivShirt);
+        btSetting = (Button) findViewById(R.id.main_activity_btSetting);
+        btGetImageGallery = (Button) findViewById(R.id.main_activity_btGetImageGallery);
+        ivImageShow = (ImageView) shapeLayout.findViewById(R.id.main_activity_ivImage);
+        ivImageShow.setTag("ImageShow");
+        ivResizeTop = (ImageView) shapeLayout.findViewById(R.id.main_activity_ivResizeTop);
+        ivResizeTop.setTag("ResizeTop");
+        ivResizeBottom = (ImageView) shapeLayout.findViewById(R.id.main_activity_ivResizeBottom);
+        ivResizeBottom.setTag("ResizeBottom");
+    }
+
+    private void showDirectionTShirt(int tShirt_direction)
     {
-        layoutParams = (LayoutParams) view.getLayoutParams();
-        switch (motionEvent.getAction())
+        tShirtDirection = tShirt_direction;
+        if(!colors.equals("white"))
         {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), tShirt_direction);
+            bitmap = utilImage.grayScaleImage(bitmap, colors);
+            ivShirt.setImageBitmap(bitmap);
+        }
+        else
+        {
+            ivShirt.setImageResource(tShirt_direction);
+        }
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.main_activity_btSetting:
+                    colors = getResources().getString(R.string.green);
+                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(),tShirtDirection);
+                    bitmap = utilImage.grayScaleImage(bitmap, colors);
+                    ivShirt.setImageBitmap(bitmap);
+                    break;
+                case R.id.main_activity_btGetImageGallery:
+                    addLayoutImage();
+                    break;
+            }
+        }
+    };
+    private void addLayoutImage()
+    {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        shapeLayout.setLayoutParams(layoutParams);
+        ivImageShow.setImageResource(R.drawable.ic_launcher);
+        ivImageShow.setOnTouchListener(onTouchListenerImage);
+        ivResizeBottom.setOnTouchListener(onTouchListenerImage);
+        ivResizeTop.setOnTouchListener(onTouchListenerImage);
+        rlRootLayout.addView(shapeLayout);
+    }
+
+    View.OnTouchListener onTouchListenerImage = new View.OnTouchListener()
+    {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent)
+        {
+            final int X = (int) motionEvent.getRawX();
+            final int Y = (int) motionEvent.getRawY();
+            if (view.getTag().equals("ImageShow"))
+            {
+                actionImageShow(motionEvent, X, Y);
+            }
+            return true;
+        }
+    };
+
+    private void actionImageShow(MotionEvent motionEvent, int x, int y)
+    {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK)
+        {
+            case MotionEvent.ACTION_DOWN:
+                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) shapeLayout.getLayoutParams();
+                _xDelta = x - lParams.leftMargin;
+                _yDelta = y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) shapeLayout.getLayoutParams();
+                layoutParams.leftMargin = x - _xDelta;
+                layoutParams.topMargin = y - _yDelta;
+                layoutParams.rightMargin = -250;
+                layoutParams.bottomMargin = -250;
+                shapeLayout.setLayoutParams(layoutParams);
+                break;
+        }
+        shapeLayout.invalidate();
+    }
+
+    private boolean onTouchShirt(View view, MotionEvent motionEvent) {
+        layoutParams = (LayoutParams) view.getLayoutParams();
+        switch (motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 home_x = (int) motionEvent.getRawX();
                 home_y = (int) motionEvent.getRawY();
--- src/com/free/tshirtdesigner/util/UtilImage.java
+++ src/com/free/tshirtdesigner/util/UtilImage.java
@@ -17,33 +17,29 @@ public class UtilImage {
         final double GS_RED = Double.parseDouble(colors[0]);
         final double GS_GREEN = Double.parseDouble(colors[1]);
         final double GS_BLUE = Double.parseDouble(colors[2]);
-
+        int width = src.getWidth();
+        int height = src.getHeight();
         // create output bitmap
-        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
-        // pixel information
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // color information
         int A, R, G, B;
         int pixel;
 
-        // get image size
-        int width = src.getWidth();
-        int height = src.getHeight();
-
-        // scan through every single pixel
+        // scan through all pixels
         for(int x = 0; x < width; ++x) {
             for(int y = 0; y < height; ++y) {
-                // get one pixel color
+                // get pixel color
                 pixel = src.getPixel(x, y);
-                // retrieve color of all channels
+                // apply filtering on each channel R, G, B
                 A = Color.alpha(pixel);
-                R = Color.red(pixel);
-                G = Color.green(pixel);
-                B = Color.blue(pixel);
-                // take conversion up to one single value
-                R = G = B = (int)(GS_RED * R + GS_GREEN * G + GS_BLUE * B);
-                // set new pixel color to output bitmap
+                R = (int)(Color.red(pixel) * GS_RED);
+                G = (int)(Color.green(pixel) * GS_GREEN);
+                B = (int)(Color.blue(pixel) * GS_BLUE);
+                // set new color pixel to output bitmap
                 bmOut.setPixel(x, y, Color.argb(A, R, G, B));
             }
         }
+
         // return final image
         return bmOut;
     }
