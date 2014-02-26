29a3ee7c2932b2e35315b52dfe71bcfc27c564ba
Dao Duc Thinh <thinhdd@qsoft.com.vn>
2014-02-26 18:37:25 +0700

    comit box

--- src/com/free/tshirtdesigner/TouchExampleView.java
+++ src/com/free/tshirtdesigner/TouchExampleView.java
@@ -1,34 +1,40 @@
 package com.free.tshirtdesigner;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class TouchExampleView extends View {
-    private Drawable mIcon;
     RelativeLayout lliMage;
     private float mPosX;
     private float mPosY;
+    private String font;
+    private String color;
+    private ImageView ivImage;
+    private TextView tvText;
 
     private VersionedGestureDetector mDetector;
     private float mScaleFactor = 1.f;
-    
+
     public TouchExampleView(Context context) {
         this(context, null, 0);
     }
-    
+
     public TouchExampleView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
-    
+
     public TouchExampleView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -40,12 +46,12 @@ public class TouchExampleView extends View {
         lliMage.measure(measureWidth, measuredHeight);
         lliMage.layout(0, 0, lliMage.getMeasuredWidth(), lliMage.getMeasuredHeight());
 
-        mIcon = context.getResources().getDrawable(R.drawable.ic_launcher);
-        mIcon.setBounds(0, 0, mIcon.getIntrinsicWidth(), mIcon.getIntrinsicHeight());
-        
+//        mIcon = context.getResources().getDrawable(R.drawable.ic_launcher);
+//        mIcon.setBounds(0, 0, mIcon.getIntrinsicWidth(), mIcon.getIntrinsicHeight());
+
         mDetector = VersionedGestureDetector.newInstance(context, new GestureCallback());
     }
-    
+
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         mDetector.onTouchEvent(ev);
@@ -55,14 +61,26 @@ public class TouchExampleView extends View {
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        
+
         canvas.save();
         canvas.translate(mPosX, mPosY);
         canvas.scale(mScaleFactor, mScaleFactor);
         lliMage.draw(canvas);
+        lliMage.setBackgroundResource(R.drawable.box_move);
 
         canvas.restore();
     }
+
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+//        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+//        this.setMeasuredDimension(parentWidth, parentHeight);
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//
+//        lliMage.layout(0, 0, parentWidth, parentHeight);
+//        lliMage.invalidate();
+//    }
 
     private class GestureCallback implements VersionedGestureDetector.OnGestureListener {
         public void onDrag(float dx, float dy) {
@@ -79,5 +97,25 @@ public class TouchExampleView extends View {
 
             invalidate();
         }
+    }
+
+    public String getFont()
+    {
+        return font;
+    }
+
+    public void setFont(String font)
+    {
+        this.font = font;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color)
+    {
+        this.color = color;
     }
 }
